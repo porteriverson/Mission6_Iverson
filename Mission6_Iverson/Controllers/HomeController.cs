6198b070c979279cf3020353b16d0a2c0cb456ce
porteriverson <live4soccer02@icloud.com>
2025-02-10 09:47:35 -0700

    Initial commit

---
+++ Mission6_Iverson/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mission6.Models;
+
+namespace Mission6.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult GetToKnow()
+    {
+        return View();
+    }
+    
+}
